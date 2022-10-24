3d1eadc995f27e697a2ff273ad0038e090b037d9
mea250802 <mea250802@gmail.com>
2022-10-24 05:18:12 +0300

    windows fix

--- run_test.sh
+++ run_test.sh
@@ -4,8 +4,10 @@ rm -f total_points.txt monsters pass.txt
 for (( count=1; count<=87; count++ ))
   do
     echo Test "$count"
-    ./test_gen 
+    ./test_gen
+    dos2unix test.CFG >& /dev/null
     timeout 30 ./little_onion
+    dos2unix result >& /dev/null
     ./test_check_log
     rm -f test.CFG
     ./test_count
